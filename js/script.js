75b91a0cffa66e1f7f637c37d0b39501e29ec776
lvivduncan <lvivduncan@gmail.com>
2021-12-12 13:27:42 +0200

    page

--- js/script.js
+++ js/script.js
@@ -100,27 +100,31 @@ document.addEventListener('keydown', event => {
 {
     const skills = document.querySelectorAll('.levus-skills')
 
-    const start = document.documentElement.clientHeight
-
-    window.addEventListener('scroll', () => {
-
-        const change = Math.floor(skills[0].getBoundingClientRect().top)
-
-        const sum = Math.floor(start - change + 10)
-
-        if (sum === 0 || sum === 1 || sum === 2 || sum === 3 || sum === 4 || sum === 5 || sum === 6 || sum === 7) {
-            skills.forEach(skill => delay(skill, skill.dataset.result, skill.dataset.speed))
+    if(skills.length > 0){
+    
+        const start = document.documentElement.clientHeight
+    
+        window.addEventListener('scroll', () => {
+    
+            const change = Math.floor(skills[0].getBoundingClientRect().top)
+    
+            const sum = Math.floor(start - change + 10)
+    
+            if (sum === 0 || sum === 1 || sum === 2 || sum === 3 || sum === 4 || sum === 5 || sum === 6 || sum === 7) {
+                skills.forEach(skill => delay(skill, skill.dataset.result, skill.dataset.speed))
+            }
+        });
+    
+        function delay(selector, num = 2, speed = 1){
+    
+            for (let index = 0; index <= num; index++) {
+    
+                setTimeout( () => {
+                    selector.innerHTML = index          
+                }, index * speed * 30)
+            }
         }
-    });
 
-    function delay(selector, num = 2, speed = 1){
-
-        for (let index = 0; index <= num; index++) {
-
-            setTimeout( () => {
-                selector.innerHTML = index          
-            }, index * speed * 30)
-        }
     }
 }
 
