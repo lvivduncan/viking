456132e3321989becdac260b1c0eb29507ff46f9
lvivduncan <lvivduncan@gmail.com>
2021-12-12 14:09:51 +0200

    articles

--- js/script.js
+++ js/script.js
@@ -258,3 +258,19 @@ document.addEventListener('keydown', event => {
         })
     })
 }
+
+
+// https://lvivduncan.github.io/levus-table/
+{
+    const table = document.querySelectorAll('table');
+
+    if (table.length > 0) {
+        table.forEach(item => {
+            const wrapper = document.createElement('div');
+            wrapper.className = 'levus-table-wrapper';
+            const table = item.cloneNode(true);
+            item.replaceWith(wrapper);
+            wrapper.append(table);
+        });
+    }
+}
